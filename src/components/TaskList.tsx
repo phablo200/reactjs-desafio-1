66f79654dcdfc33173086cc8ba33a0662fe0e0d9
Phablo Vilas Boas <phablo_sena@hotmail.com>
2022-01-03 09:09:09 -0300

    finish

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle) {  
+      const task = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        isComplete: false
+      };
+
+      setTasks([...tasks, task]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) {
+        return {
+          ...task,
+          isComplete: !task.isComplete
+        };
+      }
+      return task;
+    });
+
+    setTasks([ ...updatedTasks ]);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const updatedTasks = tasks.filter(task => task.id !== id);
+
+    setTasks([ ...updatedTasks ]);
   }
 
   return (
